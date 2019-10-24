fb89c921ce8eb99f4468c787a86a14fd0a3adcb8
Ediii96 <47295918+Ediii96@users.noreply.github.com>
2019-10-24 16:06:04 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -121,3 +121,45 @@ btn.onclick = function () {
 window.onscroll = magic;
 
 });
+
+// СЛАЙДЕР
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i=0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i=0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active1", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active1";
+
+}
+
+setInterval(function(){
+    document.getElementById("next").click(function(){
+        plusSlides(slideIndex+1);
+    });
+}, 6000);
+
+    
+
